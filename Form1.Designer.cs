54cbbd1c1a71435a90ab9dbdc9a9446ab2a88aab
Ryan87960 <zack87960@gmail.com>
2025-04-14 15:55:30 +0800

    建立類別與物件的概念
    如何加入新項目、新類別、物件

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,6 +46,8 @@
             this.btnDivide = new System.Windows.Forms.Button();
             this.btnEqual = new System.Windows.Forms.Button();
             this.btnDot = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOne
@@ -245,11 +247,31 @@
             this.btnDot.UseVisualStyleBackColor = true;
             this.btnDot.Click += new System.EventHandler(this.btnDot_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(738, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtNumber);
             this.Controls.Add(this.btnNine);
             this.Controls.Add(this.btnEqual);
@@ -295,6 +317,8 @@
         private System.Windows.Forms.Button btnDivide;
         private System.Windows.Forms.Button btnEqual;
         private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
