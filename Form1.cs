0a4ec973628410c8ba3d35e012f0b6d2c044419a
Ryan87960 <zack87960@gmail.com>
2025-04-14 14:12:16 +0800

    建立函數(Add_Number)
    建立Add_Number

--- Form1.cs
+++ Form1.cs
@@ -20,85 +20,102 @@ namespace Calculator
         {
             InitializeComponent();
         }
+        private void Add_Number(string _number)
+        {
+            if (txtNumber.Text == "0")
+                txtNumber.Text = "";
+            txtNumber.Text = txtNumber.Text + _number;
+        }
+
 
         private void btnOne_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "1";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("1");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "1";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnTwo_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "2";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("2");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "2";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnThree_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "3";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("3");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "3";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnFour_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "4";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("4");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "4";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnFive_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "5";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("5");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "5";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnSix_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "6";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("6");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "6";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnSeven_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "7";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("7");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "7";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnEight_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "8";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("8");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "8";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnNine_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "9";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("9");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "9";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnZero_Click(object sender, EventArgs e)
         {
-            if (txtNumber.Text == "0")
-                txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
-            txtNumber.Text += "0";
-            // 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
+            Add_Number("0");
+            //if (txtNumber.Text == "0")
+            //    txtNumber.Text = ""; // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            //txtNumber.Text += "0";
+            //// 這種寫法和這段「txtNumber.Text = txtNumber.Text + "1";」是一樣的
         }
 
         private void btnPlus_Click(object sender, EventArgs e)
